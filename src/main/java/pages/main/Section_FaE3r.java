079318d39440427586619c2c152d3a52afdf8874
Norayr <n.hovhannisyan.h@gmail.com>
2022-03-30 15:45:32 +0400

    New Sections

--- src/main/java/pages/main/Section_FaE3r.java
+++ src/main/java/pages/main/Section_FaE3r.java
@@ -3,5 +3,25 @@ Section - Story images
  */
 package pages.main;
 
-public class Section_FaE3r {
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.BasePage;
+
+public class Section_FaE3r extends BasePage {
+
+    String storyImageName = "https://embry.tech/wp-content/uploads/2021/02/Story-scaled.webp";
+    @FindBy(xpath = "//*[@id='column_R8oZE']//img")
+    private WebElement storyImage;
+
+    public boolean isStoryImageDisplay() {
+        boolean result = false;
+        if (!storyImageName.equals(storyImage.getAttribute("src"))) {
+            System.out.println(storyImageName + " - is not displayed");
+            result = false;
+        } else {
+            result = true;
+        }
+        return result;
+    }
 }
+
--- src/test/java/tests/Section_FaE3r_Tests.java
+++ src/test/java/tests/Section_FaE3r_Tests.java
@@ -3,5 +3,23 @@ Section - Story images
  */
 package tests;
 
-public class Section_FaE3r_Tests {
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.main.Section_FaE3r;
+import pages.main.Section_y54AO;
+
+public class Section_FaE3r_Tests extends BaseTest {
+    final String PATH = "";
+
+    @BeforeMethod
+    void openPage() {
+        navigateToURI(PATH);
+    }
+
+    @Test
+    public void tc_1() throws InterruptedException {
+        Section_FaE3r sectionFaE3r = new Section_FaE3r();
+        Assert.assertTrue(sectionFaE3r.isStoryImageDisplay());
+    }
 }
